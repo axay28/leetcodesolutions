8368bb7bae653f6c158128efcf3c848d902dc2d2
Akshay Mulgund <43311561+axay28@users.noreply.github.com>
2024-01-15 00:22:43 -0800

    Time: 1436 ms (79.90%), Space: 73 MB (12.29%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -6,10 +6,14 @@ class Solution:
             losses[loser]+=1
             players.add(winner)
             players.add(loser)
+        # print(losses)
+        # print(players)
         no_losses,one_loss=[],[]
         for player in players:
             if losses[player]==1:
                 one_loss.append(player)
+                # print(one_loss)
             elif losses[player]==0:
                 no_losses.append(player)
+                # print(no_losses)
         return [sorted(no_losses),sorted(one_loss)]
