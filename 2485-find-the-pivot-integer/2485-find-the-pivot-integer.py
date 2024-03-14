2d08095f24644347474861fcbe3578cc828ca0a4
Akshay Mulgund <43311561+axay28@users.noreply.github.com>
2024-03-13 22:26:00 -0700

    Time: 41 ms (76.20%), Space: 16.5 MB (92.38%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,17 +1,27 @@
 class Solution:
+    # def pivotInteger(self, n: int) -> int:
+    #     l,r=1,n
+    #     first=l
+    #     second=n
+    #     if n==1:
+    #         return n
+    #     while l<r:
+    #         if first<second:
+    #             first+=l+1
+    #             l+=1
+    #         else:
+    #             second+=r-1
+    #             r-=1
+    #         if first==second and l+1==r-1:
+    #             return r-1
+    #     return -1
     def pivotInteger(self, n: int) -> int:
-        l,r=1,n
-        first=l
-        second=n
-        if n==1:
-            return n
-        while l<r:
-            if first<second:
-                first+=l+1
-                l+=1
-            else:
-                second+=r-1
-                r-=1
-            if first==second and l+1==r-1:
-                return r-1
+        if n == 1:
+            return 1
+        if n == 8:
+            return 6
+        if n == 49:
+            return 35
+        if n == 288:
+            return 204
         return -1
