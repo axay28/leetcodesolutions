3fb44c051440907276a429866c14d0c2aa4da113
Akshay Mulgund <43311561+axay28@users.noreply.github.com>
2024-11-10 22:00:51 -0800

    Time: 0 ms (100.00%), Space: 20.3 MB (82.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,11 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         if not nums:
-        	return []
-        if len(nums) == 1:
-	        return [nums[0]]
-        freq_map=collections.Counter(nums)
-        h=[(-freq_map, num) for num, freq in freq_map.items()]
+            return []
+        if len(nums)==1:
+            return [nums[0]]
+        freq=collections.Counter(nums)
+        h=[(-freq,n) for n,freq in freq.items() ]
         heapq.heapify(h)
-        res = heapq.nlargest(k, freq_map.keys(), key=freq_map.get)
-        return res
+        ans=heapq.nlargest(k,freq.keys(),key=freq.get)
+        return ans
+        
